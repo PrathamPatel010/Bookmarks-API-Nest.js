0056d86c567150e7cd02da561aaed4589e364ae5
Pratham Patel <ppratham812@gmail.com>
2024-05-21 15:47:18 +0530

    defined DTOs with validation pipes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,15 +2,16 @@ import {Injectable, Req} from "@nestjs/common";
 import {User} from "@prisma/client";
 import {PrismaService} from "../prisma/prisma.service";
 import {Request} from "express";
+import {AuthDto} from "./dto";
 
 @Injectable({})
 export class AuthService{
     constructor(private prismaService:PrismaService) {
         this.prismaService = prismaService;
     }
-    signup(data:any){
-        console.log(data);
-        return data;
+    signup(userData:AuthDto){
+        console.log(userData);
+        return userData;
     }
 
     signin(){
